78367589e4b4fc3a5c7c4ce90a7e7d97d14672aa
Jana Bartecka <xcrazygirl@seznam.cz>
2023-03-10 14:03:25 +0100

    20230310 zaloha 5

--- js/filters2.js
+++ js/filters2.js
@@ -9,23 +9,23 @@ allFilters.forEach(filter => filter.addEventListener('click',() => {
 }));
 
 // show/hide filters and change text in the button
-// const btnShowFilter=document.querySelector('.button__filtrovani')
-// const filterSection=document.querySelector('.filters')
-// 
-// btnShowFilter.addEventListener('click', () => {
-    // if (btnShowFilter.textContent === "Skryt filtrovani") {
-        // btnShowFilter.textContent = "Zobrazit filtrovani"
-    // } else if (btnShowFilter.textContent === "Zobrazit filtrovani") {
-        // btnShowFilter.textContent = "Skryt filtrovani"
-    // }
-    // filterSection.classList.toggle('filters__hide')
-// });
+const btnShowFilter=document.querySelector('.button__filtrovani')
+const filterSection=document.querySelector('.filters')
+
+btnShowFilter.addEventListener('click', () => {
+    if (btnShowFilter.textContent === "Skryt filtrovani") {
+        btnShowFilter.textContent = "Zobrazit filtrovani"
+    } else if (btnShowFilter.textContent === "Zobrazit filtrovani") {
+        btnShowFilter.textContent = "Skryt filtrovani"
+    }
+    filterSection.classList.toggle('filters__hide')
+});
 
 // Load filter value from local storage if it exists
-if (localStorage.getItem("filterValue")) {
+// if (localStorage.getItem("filterValue")) {
     // filterInput.value = localStorage.getItem("filterValue");
     // console.log(localStorage.getItem("filterValue"));
-  }
+//   }
 
 const akceItem=document.querySelectorAll('.button__filtrovani')
 akceItem.forEach(akce => akce.addEventListener('click',() => {
