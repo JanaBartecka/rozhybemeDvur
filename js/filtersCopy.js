357a2cb29ade4b6e6a36c2d08e3733c337338c4d
Jana Bartecka <xcrazygirl@seznam.cz>
2023-03-16 13:21:07 +0100

    20230316 zaloha

--- js/filtersCopy.js
+++ js/filtersCopy.js
@@ -1,14 +1,11 @@
 // not exactly vanilla as there is one lodash function
 
-var allCheckboxes = document.querySelectorAll('.checkbox');
+var allCheckboxes = document.querySelectorAll('input[type=checkbox]');
 var allPlayers = Array.from(document.querySelectorAll('.akce__item'));
 var checked = {};
 
 getChecked('TypAkce');
 getChecked('DobaKonani');
-getChecked('KrouzkyPodleVeku');
-getChecked('AktivityPodleVeku');
-getChecked('Vylety');
 // getChecked('position');
 // getChecked('nbaTeam');
 // getChecked('conference');
@@ -32,14 +29,11 @@ function setVisibility() {
   allPlayers.map(function (el) {
     var TypAkce = checked.TypAkce.length ? _.intersection(Array.from(el.classList), checked.TypAkce).length : true;
     var DobaKonani = checked.DobaKonani.length ? _.intersection(Array.from(el.classList), checked.DobaKonani).length : true;
-    var KrouzkyPodleVeku = checked.KrouzkyPodleVeku.length ? _.intersection(Array.from(el.classList), checked.KrouzkyPodleVeku).length : true;
-    var AktivityPodleVeku = checked.AktivityPodleVeku.length ? _.intersection(Array.from(el.classList), checked.AktivityPodleVeku).length : true;
-    var Vylety = checked.Vylety.length ? _.intersection(Array.from(el.classList), checked.Vylety).length : true;
     // var position = checked.position.length ? _.intersection(Array.from(el.classList), checked.position).length : true;
     // var nbaTeam = checked.nbaTeam.length ? _.intersection(Array.from(el.classList), checked.nbaTeam).length : true;
     // var conference = checked.conference.length ? _.intersection(Array.from(el.classList), checked.conference).length : true;
     // if (startingReserves && injured && position && nbaTeam && conference) {
-    if (TypAkce && DobaKonani && KrouzkyPodleVeku && AktivityPodleVeku && Vylety) {
+    if (TypAkce && DobaKonani) {
       el.style.display = 'block';
     } else {
       el.style.display = 'none';
