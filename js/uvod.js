a21d379b79fc67f54e55fb2d504e4dd6ca86a50a
Jana Bartecka <xcrazygirl@seznam.cz>
2023-03-17 13:37:15 +0100

    20230317 zaloha4

--- js/uvod.js
+++ js/uvod.js
@@ -1,2 +1,19 @@
 const uvodButton=document.querySelectorAll('.uvod__button')
-console.log(uvodButton);
+
+for(let i=0; i<uvodButton.length; i++ ) {
+    uvodButton[i].addEventListener('click', (event) => {
+        const thisButton=event.target.id
+        const thisTextToHide=document.querySelector('.' + thisButton)
+        thisTextToHide.classList.toggle("uvod__vice--hide")
+        console.log(event.target.innerHTML);
+        if (event.target.innerHTML!=="Zavřít") {
+            event.target.innerHTML="Zavřít"
+        } else {
+            if (event.target.id==="uvod__button-1") {
+                event.target.innerHTML="Více o nás"
+            } else if (event.target.id==="uvod__button-2") {
+                event.target.innerHTML="Jak pomoci?"
+            }
+        }
+    })
+}
